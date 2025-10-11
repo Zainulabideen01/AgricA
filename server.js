0f80f3dfc8062f8b24b47ef089d911402ef47fbf
engr kareem A.A <abideenkareem05@gmail.com>
2025-10-11 12:40:05 +0100

    correct the static images not showing(set the path)

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const port = process.env.PORT || 3000;
 
 
 //Middleware
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 // the line below, replaces 'path/to/your/favicon.ico'
 app.use(favicon(path.join(__dirname, "public", "images", "favicon_io", 'favicon.ico')));
